66b8ecebc571fa8d4159d3ecdb7eb55316519a8f
Jhwan02 <shinjh0218@gmail.com>
2025-05-30 03:24:35 +0900

    FIX: SignupRequest dto 수정

--- Backend/recommend-diet/src/main/java/opensource_project_team6/recommend_diet/domain/user/dto/SignupRequest.java
+++ Backend/recommend-diet/src/main/java/opensource_project_team6/recommend_diet/domain/user/dto/SignupRequest.java
@@ -10,7 +10,6 @@ public record SignupRequest(
         String name,       // 이름
         String email,            // 이메일 (로그인 ID)
         String password,         // 비밀번호
-        String passwordConfirm,  // 비밀번호 확인 (서버 검증 or 프론트 확인)
         LocalDate birthDate,      // 생년월일
         Double height,
         Double weight,
