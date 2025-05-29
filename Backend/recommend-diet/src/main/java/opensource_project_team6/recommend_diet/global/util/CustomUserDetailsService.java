26260315521ba6aa7004ed24bf0d8432008c395d
Jhwan02 <shinjh0218@gmail.com>
2025-05-30 00:40:19 +0900

    FEAT: CustomUserDetail 및 UserPrincipal를 통한 사용자 인증 로직 추가

---
+++ Backend/recommend-diet/src/main/java/opensource_project_team6/recommend_diet/global/util/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package opensource_project_team6.recommend_diet.global.util;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import opensource_project_team6.recommend_diet.domain.user.entity.User;
+import opensource_project_team6.recommend_diet.domain.user.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class CustomUserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        log.info("[UserDetailsService] loadUserByUsername 호출됨. 이메일: {}", email);
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다"));
+        log.info("[UserDetailsService] 사용자 조회 성공. userId: {}, name: {}", user.getId(), user.getName());
+        return new UserPrincipal(
+                user.getId(),
+                user.getEmail(),
+                user.getName(), // 이름
+                List.of(new SimpleGrantedAuthority("ROLE_USER"))
+        );
+    }
+}
