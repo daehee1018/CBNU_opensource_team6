ca9b2ffcd0b79c2f0caec7933421d0e6153f7c17
Jhwan02 <shinjh0218@gmail.com>
2025-06-04 06:40:49 +0900

    FIX : 음식 조회 부분 securityConfig 수정

--- Backend/recommend-diet/src/main/java/opensource_project_team6/recommend_diet/global/config/SecurityConfig.java
+++ Backend/recommend-diet/src/main/java/opensource_project_team6/recommend_diet/global/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
                 .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                                 .requestMatchers(HttpMethod.POST, "/api/auth/**").permitAll()
-                                .requestMatchers("/","/oauth2/**").permitAll() // 회원가입, 로그인은 인증 불필요
+                                .requestMatchers("/","/oauth2/**","/api/food/**").permitAll() // 회원가입, 로그인은 인증 불필요
                                 .anyRequest().authenticated() // 나머지 요청은 인증 필요
                         //.anyRequest().permitAll() 모든 요청 허용
                 )
