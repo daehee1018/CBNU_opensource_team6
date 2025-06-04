9cdb3ff68795adf9941199321b4f5337d7b2ce2d
Jhwan02 <shinjh0218@gmail.com>
2025-06-04 13:59:42 +0900

    FEAT : 음식 엔티티(Food) 생성 및 영양 정보 필드 정의

---
+++ Backend/recommend-diet/src/main/java/opensource_project_team6/recommend_diet/domain/food/entity/Food.java
@@ -0,0 +1,33 @@
+package opensource_project_team6.recommend_diet.domain.food.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "food")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Food {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "food_id")
+    private Long id;
+
+    private String name; // 음식 이름
+    private String category;
+    private String repName;
+    private String subcategory;
+    private String standardAmount;
+    private String imageUrl;
+
+    private Integer energy;
+    private Double protein;
+    private Double fat;
+    private Double carbohydrate;
+    private Double sugar;
+    private Double sodium;
+    private Double cholesterol;
+    private Double saturatedFat;
+
+}
