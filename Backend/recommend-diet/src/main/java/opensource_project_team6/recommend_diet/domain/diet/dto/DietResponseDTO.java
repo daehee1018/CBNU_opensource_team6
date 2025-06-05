5c5109a57d05f0b159f8a6ffef6f7105431845df
Jhwan02 <shinjh0218@gmail.com>
2025-06-06 05:01:17 +0900

    FEAT : DietRequestDTO, DietResponseDto 추가

---
+++ Backend/recommend-diet/src/main/java/opensource_project_team6/recommend_diet/domain/diet/dto/DietResponseDTO.java
@@ -0,0 +1,22 @@
+package opensource_project_team6.recommend_diet.domain.diet.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import opensource_project_team6.recommend_diet.domain.diet.entity.MealTime;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+public class DietResponseDTO {
+    private String foodName;
+    private String imageUrl;
+    private Double amount;
+    private MealTime mealTime;
+    private LocalDate date;
+
+    private Integer energy; //칼로리
+    private Double protein;
+    private Double fat;
+    private Double carbohydrate;
+}
