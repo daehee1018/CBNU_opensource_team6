bd1324d36564bf2a11ee76218a0975153eb4caa7
Jhwan02 <shinjh0218@gmail.com>
2025-06-06 05:00:24 +0900

    FEAT : Diet 엔티티 생성 및 MealTime enum 생성

---
+++ Backend/recommend-diet/src/main/java/opensource_project_team6/recommend_diet/domain/diet/entity/Diet.java
@@ -0,0 +1,40 @@
+package opensource_project_team6.recommend_diet.domain.diet.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import opensource_project_team6.recommend_diet.domain.food.entity.Food;
+import opensource_project_team6.recommend_diet.domain.user.entity.User;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "diet")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Diet {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "diet_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "food_id")
+    private Food food;
+
+    private Double amount;
+
+    @Enumerated(EnumType.STRING)
+    private MealTime mealTime;
+
+    private LocalDate date;
+
+    private Integer energy;
+    private Double protein;
+    private Double fat;
+    private Double carbohydrate;
+}
---
+++ Backend/recommend-diet/src/main/java/opensource_project_team6/recommend_diet/domain/diet/entity/MealTime.java
@@ -0,0 +1,5 @@
+package opensource_project_team6.recommend_diet.domain.diet.entity;
+
+public enum MealTime {
+    BREAKFAST, LUNCH, DINNER
+}
