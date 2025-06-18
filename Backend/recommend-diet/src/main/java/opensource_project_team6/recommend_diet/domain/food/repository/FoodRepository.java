a29f5eb1c26d8134cf29f5a2b688d826f3dbbdd8
Jhwan02 <shinjh0218@gmail.com>
2025-06-19 07:26:36 +0900

    FEAT : 식재료명 정확한 일치 방식 검색 매서드 구현

--- Backend/recommend-diet/src/main/java/opensource_project_team6/recommend_diet/domain/food/repository/FoodRepository.java
+++ Backend/recommend-diet/src/main/java/opensource_project_team6/recommend_diet/domain/food/repository/FoodRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 public interface FoodRepository extends JpaRepository<Food, Long> {
     List<Food> findByNameContaining(String keyword); // 특정 키워드가 포함된 음식을 검색 ex) "제육" 검색 -> 제육볶음, 제육덮밥 반환
     Optional<Food> findByName(String name); // 음식 이름으로 정확히 검색
+    List<Food> findBySubcategory(String subcategory);
 }
