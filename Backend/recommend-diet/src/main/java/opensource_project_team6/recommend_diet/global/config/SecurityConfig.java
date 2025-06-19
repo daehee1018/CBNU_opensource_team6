6319a79f1102cabf3e9193992f1bbdb7ceff55e0
YHFTF <yunghy1@chungbuk.ac.kr>
2025-06-19 23:19:08 +0900

    업로드 용량 제한

--- Backend/recommend-diet/src/main/java/opensource_project_team6/recommend_diet/global/config/SecurityConfig.java
+++ Backend/recommend-diet/src/main/java/opensource_project_team6/recommend_diet/global/config/SecurityConfig.java
@@ -54,6 +54,7 @@ public class SecurityConfig {
                                 .requestMatchers(HttpMethod.POST, "/api/auth/**").permitAll()
                                 .requestMatchers("/","/oauth2/**","/api/food/**").permitAll() // 회원가입, 로그인은 인증 불필요
                                 .requestMatchers(HttpMethod.GET, "/api/user/**").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/api/diet/image/preview").permitAll()
                                 .anyRequest().authenticated() // 나머지 요청은 인증 필요
                         //.anyRequest().permitAll() 모든 요청 허용
                 )
